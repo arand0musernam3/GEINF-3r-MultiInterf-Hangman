a8d85b21ef31b7a19d2c43393707d63e5385fcf2
arand0musernam3 <anjuanolavil@gmx.com>
2023-10-18 17:16:23 +0200

    Avançat Hangman

--- hangman.js
+++ hangman.js
@@ -1,9 +1,15 @@
 //VECTOR OF AVAILABLE WORDS IN GAME
-const words = ["figma", "balls", "house", "car"];
+const words = ["caca"];
 const word = words[Math.floor(Math.random() * words.length)];
 var guessed = "";
 var misses = [];
 var imageNum = 0;
+
+//https://stackoverflow.com/questions/1431094/how-do-i-replace-a-character-at-a-particular-index-in-javascript
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substring(0,index) + chr + str.substring(index+1);
+}
 
 
 function updateGuessed() {
@@ -22,11 +28,17 @@ function updateImage() {
 }
 
 function startGame() {
+    player_name = document.getElementById("name_input").value;
+
+    if (player_name === "") {
+        alert("El nom del jugador no pot ser buit.");
+        return false;
+    }
+
     document.getElementById("player_name").style.display = 'none';
     document.getElementById("game").style.display = 'flex';
 
     //ajustar caracters
-    player_name = document.getElementById("name_input").value;
     document.getElementById("player_name_header").innerHTML = player_name;
 
     //llargada dels _ _ _
@@ -37,9 +49,24 @@ function startGame() {
     updateGuessed();
 }
 
+function addMiss(char) {
+    let doc = document.getElementById("wrong_chars");
+    let para = document.createElement("p");
+    para.innerHTML = char;
+    doc.appendChild(para);
+}
+
 function validateCharacter() {
     //TODO: COMPROVAR QUE NO HAGUEM FET SERVIR AQUESTA LLETRA ABANS
-    let char = document.getElementById("guessed_char").value[0];
+    let doc = document.getElementById("guessed_char");
+    let char = doc.value[0];
+
+    if (doc.value === "") {
+        alert("El caràcter no pot ser buit.");
+        return false;
+    }
+
+    doc.value = "";
     let finish = false;
     let i = 0;
     while (i < word.length) {
@@ -48,12 +75,13 @@ function validateCharacter() {
         if (word.charAt(i) == char) {
             console.log("hello");
             finish = true;
-            guessed[i] = char; //TODO: fix
+            guessed = setCharAt(guessed, i, char);
         }
         i++;
     }
     if (finish == false) {
         misses.push(char);
+        addMiss(char);
         updateImage();
     } else {
         updateGuessed();
